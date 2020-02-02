fb511c8a6d14344bb908f898c55001a5032b50b7
miura39 <miurast.miura.y@gmail.com>
2020-02-02 16:19:37 +0900

    メニューページの作成

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,4 +2,6 @@ class NewsController < ApplicationController
 
   def index
   end
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   resources :posts, only: [:new, :create, :edit]
-  resources :news, only: [:index]
+  resources :news, only: [:index, :new]
 end
